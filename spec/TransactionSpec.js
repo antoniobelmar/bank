f35331fad30f8f80710fd4054555025f7ac76bea
antonio <antoniobelmardacosta@gmail.com>
2018-01-02 14:43:58 +0000

    Add date, credit and debit properties to transaction object

--- spec/TransactionSpec.js
+++ spec/TransactionSpec.js
@@ -2,12 +2,44 @@ describe('Transaction', function() {
   var transaction;
 
   beforeEach(function() {
-    transaction = new Transaction;
+    transaction = new Transaction("01-01-2018");
   });
 
   describe('Is a constructor', function() {
     it('Can be initialized', function() {
       expect(transaction).toBeDefined()
     });
+  })
+
+  describe('Has a date property', function() {
+    it('Sets date property to the argument of constructor', function() {
+      expect(transaction._date).toEqual("01-01-2018")
+    })
+  })
+
+  describe('It has a debit property', function() {
+    it('Debit starts at 0', function() {
+      expect(transaction._debit).toEqual(0)
+    })
+    it('Has a getter that returns debit amount', function() {
+      expect(transaction.getDebit()).toEqual(transaction._debit)
+    })
+    it('Has a setter to update debit amount', function(){
+      transaction.addDebit(10)
+      expect(transaction._debit).toEqual(10)
+    })
+  })
+
+  describe('It has a credit property', function() {
+    it('Credit starts at 0', function() {
+      expect(transaction._credit).toEqual(0)
+    })
+    it('Has a getter that returns credit amount', function() {
+      expect(transaction.getCredit()).toEqual(transaction._credit)
+    })
+    it('Has a setter to update credit amount', function(){
+      transaction.addCredit(5)
+      expect(transaction._credit).toEqual(5)
+    })
   })
 })
--- src/Transaction.js
+++ src/Transaction.js
@@ -1,3 +1,21 @@
-function Transaction() {
-  
-}
+function Transaction(date = new Date()) {
+  this._date = date;
+  this._debit = 0;
+  this._credit = 0;
+};
+
+Transaction.prototype.getDebit = function() {
+  return this._debit;
+};
+
+Transaction.prototype.getCredit = function() {
+  return this._credit;
+};
+
+Transaction.prototype.addDebit = function(amount) {
+  this._debit += amount;
+};
+
+Transaction.prototype.addCredit = function(amount) {
+  this._credit += amount;
+};
