955bc4b665a9506ac502dbdcc97518e5550596a3
antonio <antoniobelmardacosta@gmail.com>
2018-01-02 18:32:57 +0000

    Add function to print statement to the console

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -1,8 +1,10 @@
 describe("Statement", function() {
-  var statement;
+  var statement, account, transaction, transaction2;
 
   beforeEach(function(){
-    account = jasmine.createSpyObj("account",["getBalance", "getTransactionList"])
+    account = jasmine.createSpyObj("account",["getTransactionList"])
+    transaction = jasmine.createSpyObj("transaction", ["getDate", "getCredit", "getDebit"])
+    transaction2 = jasmine.createSpyObj("transaction2", ["getDate", "getCredit", "getDebit"])
     statement = new Statement(account);
   });
 
@@ -15,6 +17,33 @@ describe("Statement", function() {
   describe("Has an account property", function() {
     it('Sets account property to the argument of constructor', function(){
       expect(statement._account).toEqual(account)
+    })
+    it('Has a getter that returns the account property', function() {
+      expect(statement.getAccount()).toEqual(statement._account)
+    })
+  })
+
+  describe("Print statement", function() {
+    it('Prints the statement headers to the console', function(){
+      account.getTransactionList.and.returnValue([transaction])
+      expect(statement.print()).toContain("Date || Credit || Debit || Balance")
+    })
+    it('Prints the correct statement for one transaction', function() {
+      account.getTransactionList.and.returnValue([transaction])
+      transaction.getDate.and.returnValue("01-01-2018")
+      transaction.getCredit.and.returnValue(5)
+      transaction.getDebit.and.returnValue(0)
+      expect(statement.print()).toContain("01-01-2018 || 5 || 0 || -5")
+    })
+    it('Prints the correct statement for mutiple transactions', function() {
+      account.getTransactionList.and.returnValue([transaction, transaction2])
+      transaction.getDate.and.returnValue("01-01-2018")
+      transaction.getCredit.and.returnValue(5)
+      transaction.getDebit.and.returnValue(0)
+      transaction2.getDate.and.returnValue("02-01-2018")
+      transaction2.getCredit.and.returnValue(0)
+      transaction2.getDebit.and.returnValue(10)
+      expect(statement.print()).toContain("01-01-2018 || 5 || 0 || -5" + "\n" + "02-01-2018 || 0 || 10 || 5")
     })
   })
 });
--- src/Statement.js
+++ src/Statement.js
@@ -1,3 +1,17 @@
 function Statement(account) {
   this._account = account;
 };
+
+Statement.prototype.getAccount = function() {
+  return this._account
+}
+
+Statement.prototype.print = function() {
+  var statement = "Date || Credit || Debit || Balance";
+  var balance = 0;
+  this.getAccount().getTransactionList().forEach(function(transaction) {
+    balance +=(-transaction.getCredit() + transaction.getDebit())
+    statement+= "\n" + (transaction.getDate() + " || " + transaction.getCredit() + " || " + transaction.getDebit()) + " || " + balance
+  })
+  return statement;
+}
